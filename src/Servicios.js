ae2dcb857dfc4d84863ce4cce2e05c71d9f09e28
yuli-reyes <viviana1236@gmail.com>
2021-07-10 11:04:54 -0500

    Se hicieron cambios en la pagina de Servicios

--- src/Servicios.js
+++ src/Servicios.js
@@ -12,30 +12,32 @@ function Servicios(){
                 <h1>Servicios</h1>
                 <hr></hr>
             </article>
-            <div className="cajageneral">
-            	<article className="cajatext">
-            	<img src={paciente} alt="imagen paciente"/>
-                    <h5>Paciente</h5>                
-                    <p>This is a longer card with supporting text below as a 
-                    natural lead-in to additional content. This content is a 
-                    little bit longer.</p>
-              	</article>
-            
-              	<article className="cajatext">
-                <img src={vacunacion} alt="imagen vacunacion"/>
-                    <h5>Servicio Médico</h5>
-                    <p>This is a longer card with supporting text below as a 
-                    natural lead-in to additional content. This content is a 
-                    little bit longer.</p>
-                </article>
+            <div className="cajageneral text-center">
+                <div className="row">
+                	<article className="cajatext col-12 col-md-4">
+                	<img src={paciente} alt="imagen paciente"/>
+                        <h5>Paciente</h5>                
+                        <p>This is a longer card with supporting text below as a 
+                        natural lead-in to additional content. This content is a 
+                        little bit longer.</p>
+                  	</article>
+                
+                  	<article className="cajatext col-12 col-md-4">
+                    <img src={vacunacion} alt="imagen vacunacion"/>
+                        <h5>Servicio Médico</h5>
+                        <p>This is a longer card with supporting text below as a 
+                        natural lead-in to additional content. This content is a 
+                        little bit longer.</p>
+                    </article>
 
-                <article className="cajatext">
-                    <img src={veterinario} alt="imagen vaterinario"/>
-                    <h5>Vacunación</h5>
-                    <p>This is a longer card with supporting text below as a 
-                    natural lead-in to additional content. This content is a 
-                    little bit longer</p>
-                </article>
+                    <article className="cajatext col-12 col-md-4">
+                        <img src={veterinario} alt="imagen vaterinario"/>
+                        <h5>Vacunación</h5>
+                        <p>This is a longer card with supporting text below as a 
+                        natural lead-in to additional content. This content is a 
+                        little bit longer</p>
+                    </article>
+                </div>
             </div>
     </section>
     </div>
